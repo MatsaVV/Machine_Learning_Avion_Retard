345fb658dfdcb005b5d7a5395e3f5da156840bd3
Mathieu SAVOURE <157049199+MatsaVV@users.noreply.github.com>
2024-06-10 15:59:33 +0200

    ajout dernière version

--- app/str_app.py
+++ app/str_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import joblib
 import pandas as pd
+import os
+print("Répertoire de travail actuel :", os.getcwd())
 
-# Charger le modèle
-model = joblib.load('model.joblib')
+model = joblib.load('model/model.joblib')
 
-# Fonction pour faire une prédiction
+
 def predict_delay(day_of_month, day_of_week, crs_dep_time, dest_airport_id, crs_arr_time, origin_airport_id):
     input_data = pd.DataFrame({
         'DAY_OF_MONTH': [day_of_month],
@@ -18,15 +19,15 @@ def predict_delay(day_of_month, day_of_week, crs_dep_time, dest_airport_id, crs_
     prediction = model.predict(input_data)
     return prediction[0]
 
-# Interface utilisateur Streamlit
+
 st.title('Prédiction de Retard de Vol')
 
+day_of_week = st.number_input('Jour de la Semaine : ex Lundi = 1', min_value=1, max_value=7, step=1)
 day_of_month = st.number_input('Jour du Mois', min_value=1, max_value=31, step=1)
-day_of_week = st.number_input('Jour de la Semaine', min_value=1, max_value=7, step=1)
-crs_dep_time = st.number_input('Heure de Départ CRS', min_value=0, max_value=2359, step=1)
-dest_airport_id = st.number_input('ID de l’Aéroport de Destination', min_value=0, step=1)
-crs_arr_time = st.number_input('Heure d’Arrivée CRS', min_value=0, max_value=2359, step=1)
+crs_dep_time = st.number_input('Heure de Départ vol', min_value=0, max_value=2359, step=1)
 origin_airport_id = st.number_input('ID de l’Aéroport d’Origine', min_value=0, step=1)
+crs_arr_time = st.number_input('Heure d’Arrivée vol', min_value=0, max_value=2359, step=1)
+dest_airport_id = st.number_input('ID de l’Aéroport de Destination', min_value=0, step=1)
 
 if st.button('Prédire'):
     result = predict_delay(day_of_month, day_of_week, crs_dep_time, dest_airport_id, crs_arr_time, origin_airport_id)
